45528054995ff60d83aa6d4b8a91bfdc3edd4634
GTShri <shrika0010@gmail.com>
2025-02-17 11:05:40 +0530

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:otpp/services.dart';
 
+
+import 'emailauth/auth/login_screen.dart';
 import 'hi.dart';
-//import 'package:flutter_firebase_auth/welcome.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -19,7 +20,8 @@ class App extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text("Login"),),
-        body: const MyApp(),
+        body: const LoginScreen()
+        //MyApp(),
       ),
     );
   }
